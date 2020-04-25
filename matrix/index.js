7a1d240267d2ea2302b5911876ed8edb933d221f
Kenta Kodashima <kentakodashima@gmail.com>
2020-04-24 23:45:08 -0700

    Solve matrix.

--- matrix/index.js
+++ matrix/index.js
@@ -9,14 +9,53 @@
 //     [[1, 2, 3],
 //     [8, 9, 4],
 //     [7, 6, 5]]
-//  matrix(4)
-//     [[1,   2,  3, 4],
+//   matrix(4)
+//     [[1,  2,  3, 4],
 //     [12, 13, 14, 5],
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
 function matrix(n) {
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+  let counter = 1
+  let startCol = 0
+  let endCol = n - 1
+  let startRow = 0
+  let endRow = n - 1
+
+  // if (n < 1) return [[1]]
+
+  while (startCol <= endCol && startRow <= endRow) {
+    for (let i = startCol; i <= endCol; i++) {
+      result[startRow][i] = counter
+      counter++
+    }
+    startRow++
   
+    for (let i = startRow; i <= endRow; i++) {
+      result[i][endCol] = counter
+      counter++
+    }
+    endCol--
+  
+    for (let i = endCol; i >= startCol; i--) {
+      result[endRow][i] = counter
+      counter++
+    }
+    endRow--
+
+    for (let i = endRow; i >= startRow; i--) {
+      result[i][startCol] = counter
+      counter++
+    }
+    startCol++
+  }
+
+  return result
 }
 
 module.exports = matrix;
