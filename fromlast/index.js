ea98d3031b9fe1ee46704ab8c87d7dac63d28dee
Kenta Kodashima <kentakodashima@gmail.com>
2020-07-16 21:22:56 -0700

    Solve fromLast().

---
+++ fromlast/index.js
@@ -0,0 +1,50 @@
+// --- Directions
+// Given a linked list, return the element n spaces
+// from the last node in the list.  Do not call the 'size'
+// method of the linked list.  Assume that n will always
+// be less than the length of the list.
+// --- Examples
+//    const list = new List();
+//    list.insertLast('a');
+//    list.insertLast('b');
+//    list.insertLast('c');
+//    list.insertLast('d');
+//    fromLast(list, 2).data // 'b'
+
+function fromLast(list, n) {
+  let counter = 0
+  let fast = list.head
+  let slow = list.head
+
+  while (counter < n) {
+    fast = fast.next
+    counter++
+  }
+
+  while (fast.next) {
+    slow = slow.next
+    fast = fast.next
+  }
+
+  return slow
+}
+
+// Ver 2
+// function fromLast2(list, n) {
+//   let fast = list.getFirst()
+//   let slow = list.getFirst()
+
+//   while (n > 0) {
+//     fast = fast.next
+//     n--
+//   }
+
+//   while (fast.next) {
+//     slow = slow.next
+//     fast = fast.next
+//   }
+
+//   return slow
+// }
+
+module.exports = fromLast;
